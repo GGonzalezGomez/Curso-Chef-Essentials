75e87c8bf5c24fdfc88ba7630df8a7128a3225e7
Guillermo <Guillermo@example.com>
2017-05-15 16:12:15 +0000

    Templates added

--- recipes/server.rb
+++ recipes/server.rb
@@ -7,8 +7,8 @@ package 'httpd' do
    action :install
 end
 
-file '/var/www/html/index.html' do
-   content "<h1>Hello, world!</h1>\n"
+template '/var/www/html/index.html' do
+   source "index.html.erb"
 end
 
 service 'httpd' do
